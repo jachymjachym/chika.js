bc101e8df90628f7cb91440efbd7f057564513f5
jachym <jachymkoudela@gmail.com>
2017-03-02 17:26:23 +0100

    znova

--- src/jk-base.js
+++ src/jk-base.js
@@ -3,74 +3,96 @@
  * 
  */
 
-var requirements = {
-    touchSlide: require('./touchslide/jk-touch-slide.js')
-}
-
-
-var j$ = function (selector, index){
-
-    this.selector = selector;
-    this.index = index;
-
-    if(typeof index != 'undefined'){
-        this.element = document.querySelectorAll(this.selector)[this.index];
-    } else {
-        this.element = document.querySelectorAll(this.selector);
-    }
-
-    this.consoleElement = function(){
-        console.log(this.element);
-    }
-
-    this.attr = function(name, value){
-        if(!value) return this.element.getAttribute('name');
-        this.element.setAttribute(name, value);
-        return this;
-    }
-
-    this.on = function(type, callback){
-        var i;
-        var el = this.element;
-        if(typeof index != -1){
-            if(el == null || typeof(el) === 'undefined') return;
-            if (el.addEventListener) {
-                el.addEventListener(type, callback, false);
-            } else if (el[i].attachEvent) {
-                el.attachEvent("on" + type, callback);
-            } else {
-                el["on"+type] = callback;
-            }
-        } else {
-            for(i=0; i < el.length; i++){
-                if(el[i] == null || typeof(el[i]) === 'undefined') return;
-                if (el[i].addEventListener) {
-                    el[i].addEventListener(type, callback, false);
-                } else if (el[i].attachEvent) {
-                    el[i].attachEvent("on" + type, callback);
-                } else {
-                    el[i]["on"+type] = callback;
-                }
-            }
+var promenna = (function(window, document){
+    console.log('neco');
+    window.addEventListener('click', function(){
+        alert('klikam');
+    });
+    window = this;
+    this.obj1 = {
+        obj2: function(hoes){
+            console.log(hoes);
         }
     }
-
-    return this;
-};
-
-var jk = function(selector, index){
-    return new j$(selector, index);
-};
-
-
-for (var property in requirements) {
-    if (requirements.hasOwnProperty(property)) {
-        j$.prototype[property] = requirements[property].fn;
-    }
     
-}
+})(window, document);
 
-module.exports = jk;
+module.exports = promenna;
+
+
+
+//var requirements = {
+//    touchSlide: require('./touchslide/jk-touch-slide.js')
+//}
+
+
+//var j$ = function (selector, index){
+//
+//    this.selector = selector;
+//    this.index = index;
+//
+//    if(typeof index != 'undefined'){
+//        this.element = document.querySelectorAll(this.selector)[this.index];
+//    } else {
+//        this.element = document.querySelectorAll(this.selector);
+//    }
+//
+//    this.consoleElement = function(){
+//        console.log(this.element);
+//    }
+//
+//    this.attr = function(name, value){
+//        if(!value) return this.element.getAttribute('name');
+//        this.element.setAttribute(name, value);
+//        return this;
+//    }
+//
+//    this.on = function(type, callback){
+//        var i;
+//        var el = this.element;
+//        if(typeof index != -1){
+//            if(el == null || typeof(el) === 'undefined') return;
+//            if (el.addEventListener) {
+//                el.addEventListener(type, callback, false);
+//            } else if (el[i].attachEvent) {
+//                el.attachEvent("on" + type, callback);
+//            } else {
+//                el["on"+type] = callback;
+//            }
+//        } else {
+//            for(i=0; i < el.length; i++){
+//                if(el[i] == null || typeof(el[i]) === 'undefined') return;
+//                if (el[i].addEventListener) {
+//                    el[i].addEventListener(type, callback, false);
+//                } else if (el[i].attachEvent) {
+//                    el[i].attachEvent("on" + type, callback);
+//                } else {
+//                    el[i]["on"+type] = callback;
+//                }
+//            }
+//        }
+//    }
+//
+//    return this;
+//};
+//
+//var jk = function(selector, index){
+//    return new j$(selector, index);
+//};
+//
+//
+//for (var property in requirements) {
+//    if (requirements.hasOwnProperty(property)) {
+//        j$.prototype[property] = requirements[property].fn;
+//    }
+//    
+//}
+
+
+
+
+
+//module.exports = jk;
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
-var jk =  require('./jk-base.js');
+//var jk =  require('./jk-base.js');
+//
+//jk('.wrapper', 0).on('touchmove', function(e){
+//                
+//            })
+//            
+//          
+//jk('.slide-wrapper', 1).touchSlide();
+//
+//
+////helper function
+//function $$(selector, context) {
+//    context = context || document;
+//    var elements = context.querySelectorAll(selector);
+//    return Array.prototype.slice.call(elements);
+//} 
+//
+//window.addEventListener("scroll", function() {
+//   var scrolledHeight= window.pageYOffset;
+//  $$(".slide-wrapper").forEach(function(el,index,array) { 
+//      console.log(el);
+//      console.log(index);
+//      console.log(array);
+//    var limit= el.offsetTop+ el.offsetHeight;
+//  if(scrolledHeight > el.offsetTop && scrolledHeight <= limit) {
+//    el.style.backgroundPositionY=  (scrolledHeight - el.offsetTop) /1.5+ "px";
+// 
+//    } else {
+//     el.style.backgroundPositionY=  "0";
+//    }
+//     });
+//});
+//
+//console.log($$('.slide-wrapper'));
 
-jk('.wrapper', 0).on('touchmove', function(e){
-                
-            })
-            
-          
-jk('.slide-wrapper', 1).touchSlide();
+var promenna = require('./jk-base.js');
+
+obj1.obj2('hoes')
